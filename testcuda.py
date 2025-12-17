3c04d0a6167eb71135bdcbae3793efe1128caa75
mouhib romdhane <romdan.mouhib@gmail.com>
2025-12-17 23:38:58 +0100

    Confirm deletion of files

--- testcuda.py
+++
@@ -1,9 +0,0 @@
-import torch
-import torchvision
-print(f"Torch: {torch.__version__}")
-print(f"Vision: {torchvision.__version__}")
-print(f"CUDA Available: {torch.cuda.is_available()}")
-
-# This specific line checks if the 'nms' operator is fixed
-ops = torchvision.ops
-print("Success! Torchvision operators are found.")
